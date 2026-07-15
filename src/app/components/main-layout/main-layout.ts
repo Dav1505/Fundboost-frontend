e93184f3a1653d6ed98be28745092c048a042a49
Davide Guarini <davideguarini15@gmail.com>
2026-07-15 11:51:04 +0200

    Aggiunto bottone nella sidenav bar per permettere il logout dell'utente

--- src/app/components/main-layout/main-layout.ts
+++ src/app/components/main-layout/main-layout.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-
+import Keycloak from 'keycloak-js';
 @Component({
   selector: 'app-main-layout',
   standalone: true,
@@ -29,4 +29,12 @@ export class MainLayout {
     { path: '/projects/create', label: 'Crea progetto', icon: 'add_circle' },
     { path: '/wallet', label: 'Wallet', icon: 'account_balance_wallet' },
   ];
+
+  private readonly keycloak = inject(Keycloak);
+
+  logout(): void{
+    this.keycloak.logout({
+      redirectUri: window.location.origin
+    });
+  }
 }
